0e2af4929e793c02fc74ffecade3c83512a95194
Chris Watson <chris.watson@7digital.com>
2013-01-28 17:28:29 +0000

    Add tests for bells up to 8

--- BellringingKata/BellringingKataTests.cs
+++ BellringingKata/BellringingKataTests.cs
@@ -10,6 +10,12 @@ namespace BellringingKata
 
 		[TestCase(0, 1)]
 		[TestCase(1, 2)]
+		[TestCase(2, 3)]
+		[TestCase(3, 4)]
+		[TestCase(4, 5)]
+		[TestCase(5, 6)]
+		[TestCase(6, 7)]
+		[TestCase(7, 8)]
 		public void AssertIsExpectedBell(int bellIndex, int bellNumber)
 		{
 			var actual = GetBells().ElementAt(bellIndex);
@@ -19,8 +25,8 @@ namespace BellringingKata
 
 		private IEnumerable<int> GetBells()
 		{
-			yield return 1;
-			yield return 2;
+			var currentBell = 1;
+			while (true) yield return currentBell++;
 		}
 	}
 }
